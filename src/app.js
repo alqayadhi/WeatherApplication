535665e2064a0fbe465661b33064f428ea9b9d27
Alqayadhi <mo.alqayadhi@gmail.com>
2022-12-05 16:28:31 +0100

    essantial file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
-console.log('hello');
+
+const apiKey = '49cc8c821cd2aff9af04c9f98c36eb74';
+
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lon = position.coords.longitude,
+            lat = position.coords.latitude;
+
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=${apiKey}`;
+            //https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+        
+            fetch(url)
+            .then(res => res.json())
+            .then((data) =>{
+                console.log(data);
+                weatherReport(data);
+            })
+        })
+    }
+})
+
+function weatherReport(data){
+    const urlCast = `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apiKey}`;
+    
+    fetch(urlCast)
+    .then(res => { 
+        return res.json()})
+    .then((forecast) =>{
+        console.log(forecast);
+        hourForecast(forecast);
+        dayForecast(forecast);
+
+        document.getElementById('city').innerHTML = data.name+ ', ' + data.sys.country;
+
+        document.getElementById('temperature').innerHTML = Math.floor(data.main.temp - 273)+ ' °C';
+    })
+}
+
+function hourForecast(forecast) {
+
+}
+
+function dayForecast(forecast) {
+
+}
